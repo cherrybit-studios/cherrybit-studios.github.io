7cad81a5ab4517e33be0fcbd99f578b8bb301aaf
Erick <erickzanardoo@gmail.com>
2022-11-29 14:02:46 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,10 +34,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/com.example.verygoodcore/cherry_bit_site/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
