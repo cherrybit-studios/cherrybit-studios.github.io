393234450cef0c4038177ad9721a7a190c02990c
Erick Zanardo <erickzanardoo@gmail.com>
2023-06-29 19:20:13 -0300

    adding retro game journal

---
+++ docs/retro_game_journal.md
@@ -0,0 +1,22 @@
+---
+sidebar_position: 2
+---
+
+# Retro game journal 📔
+
+<img src="/img/retro_game_journal.png" style={{width: 600, paddingRight: 8}} />
+
+As a game developer, playing games is not only a passion by a school, by playing retro games
+I can get inspiration, learn new game design and on top of all, have fun.
+
+Retro game journal is about recording games that I beat, found interesting and worth registering
+their highlights, so it will server as both as a journal from the games that I enjoyed and learned
+from, but also, hopefully, the information in them will help fellow gamers/game developers.
+
+All issues will follow the same format, where in a single page, a brief summary and the highlights
+of the game will be found, the idea is for the content to be short and to the point and just live
+as a reminder and a call up to a fun, interesting game.
+
+## Download the issues:
+
+[Issue #1 Tiny Toon Adventures: Buster's Hidden Treasure (29/06/2023)](/retro_game_journal/RGJ_Issue_1.pdf)
--- src/pages/components/game-list.tsx
+++ src/pages/components/game-list.tsx
@@ -22,8 +22,11 @@ function GameInfo({ title, img, description, link }: Game) {
   return (
     <div className={clsx('col col--4')}>
       <Link to={link}>
-        <div className="text--center">
-          <img src={img} width="200" />
+
+        <div className="game-logo-container">
+        <div className="game-logo">
+          <img src={img} />
+        </div>
         </div>
       </Link>
       <div className="text--center padding-horiz--md">
@@ -53,6 +56,20 @@ const Games: Game[] = [
         A mix of a game and pixel art editor, play as Buddy a charming brush
         that likes to paint and create the most amazing pixel sprites. Play to
         relax, to compete and to share your art with the community!
+      </>
+    ),
+  },
+  {
+    title: 'Retro Game Journal',
+    img: 'img/retro_game_journal.png',
+    link: 'docs/retro_game_journal',
+    description: (
+      <>
+        Not a game, but rather a writting project.
+        A journal where a brief summary and the highlights of a retro
+        game is written down, it is a record of the games that I played and found out
+        interesting, to a point where I want to remember it, and hopefully
+        it can also be a call up to other people to play it too.
       </>
     ),
   },
